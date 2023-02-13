c5b4a9c54c244fbbb5bf505c4e138f544f4d03aa
Wale Obied <waleobied10@gmail.com>
2023-02-13 16:57:15 +0100

    ts listo

--- src/app/Components/componente-principal/componente-principal.component.ts
+++ src/app/Components/componente-principal/componente-principal.component.ts
@@ -14,6 +14,21 @@ export class ComponentePrincipalComponent {
   fecha: string = "";
   arrNoticias: Noticia[] = [];
   noticiasPublicadas: string = "";
+  publiTitulo: string = "";
+  publiImg: string = "";
+  publiTextoNoticia: string = "";
+  publiFecha: string = "";
+
+  constructor() { 
+    this.arrNoticias = new Array (
+      {titulo: 'Día Internacional de la Mujer y la Niña en la Ciencia', img: 'assets/11F.jpg', textoNoticia: 'El 11 de febrero se celebra el Día Internacional de la Mujer y la Niña en la Ciencia, proclamado por la ONU en el año 2015. El objetivo principal de este día es lograr una mayor participación e inclusión de las mujeres y las niñas en el mundo de la ciencia y la tecnología y de esta manera romper con la brecha de género. Actualmente, en algunos países del mundo existe una lucha de géneros, donde las mujeres han sido relegadas en la participación en disciplinas como la tecnología, las ciencias o las matemáticas.', fecha: '11/02/2023'},
+      {titulo: 'Más hallazgos del James Webb se esperan para este año', img: 'assets/JamesWebb.jpg', textoNoticia: 'Si 2022 fue el año del James Webb, 2023 también lo será. El nuevo telescopio espacial, fruto de la colaboración de la NASA, la Agencia Espacial Europea (ESA) y la Agencia Espacial Canadiense ha cumplido un año en el espacio, tiempo durante el que ha mostrado sus grandes capacidades para ver mejor y más lejos en el universo, como si fuera una máquina del tiempo. Nos ha mostrado las galaxias más antiguas conocidas y ha atisbado atmósfera del planetas extrasolares. Este año se espera que sus hallazgos científicos continúen.', fecha: '10/02/2023'}
+    ) 
+
+    console.log(this.arrNoticias)
+
+  }
+
 
   publicar(): void {
 
@@ -23,8 +38,16 @@ export class ComponentePrincipalComponent {
       textoNoticia: this.textoNoticia,
       fecha: this.fecha
     }
-    this.arrNoticias.push(noticia);
-    this.cargarNoticia();
+
+     if (this.titulo === "" || this.img ==="" || this.textoNoticia ==="" || this.fecha ===""){
+      alert("debes rellenar todos los campos.");
+
+      } else {
+        this.arrNoticias.push(noticia);
+        this.cargarNoticia();
+
+      }
+    
 
 
     console.log(this.arrNoticias);
@@ -35,10 +58,31 @@ export class ComponentePrincipalComponent {
     this.fecha = "";
   }
 
+
+  cargarPublicacion(): void {
+    this.noticiasPublicadas = "";
+    this.publiTitulo = "";
+    this.publiImg = "";
+    this.publiTextoNoticia = "";
+    this.publiFecha = "";
+
+    
+  }
+
+
   cargarNoticia(): void {
     this.noticiasPublicadas = "";
     this.arrNoticias.forEach(noticia => {
-      this.noticiasPublicadas += `<li> ${noticia.titulo} ${noticia.img} ${noticia.textoNoticia} ${noticia.fecha}</li>`;
+      this.noticiasPublicadas += 
+      `<li>
+       <h3>${noticia.titulo}</h3>
+        <img src= "${noticia.img}">
+        <p>${noticia.textoNoticia}</p>
+        <p>${noticia.fecha}</p>
+        </li>`;
     });
+    
   }
+
+
 }
